ba53981d471563263165eaeb953a1dc376309a85
Sutirtha Marjit <sutirtha.social.marjit@gmail.com>
2019-10-31 21:51:32 +0530

    test05

--- master.build.js
+++ master.build.js
@@ -5,11 +5,12 @@ const webappBuild = 'npm run build';
 const childProcess = require('child_process');
 const root = "./";
 
-console.log('test04');
+console.log('FrontEnd dependency installation:Started');
 childProcess.execSync(npmInstall,{cwd:frontEndFolderPath,env:process.env,stdio:'inherit'});
-
-
+console.log('FrontEnd dependency installation:End');
+console.log('FrontEnd build :started');
 childProcess.execSync(webappBuild,{cwd:frontEndFolderPath,env:process.env,stdio:'inherit'});
+console.log('FrontEnd build :end');
 //childProcess.execSync(npmInstall,{cwd:root,env:process.env,stdio:'inherit'});
-console.log('test04-2');
+console.log('test05');
 startApp();
