d3b90e363623c7dff37267da9adddd6af73d365e
Sutirtha Marjit <sutirtha.social.marjit@gmail.com>
2019-10-31 21:39:30 +0530

    test02

--- master.build.js
+++ master.build.js
@@ -1,10 +1,10 @@
 const startApp = require('./start');
-/*const frontEndFolderPath = './frontend/';
+const frontEndFolderPath = './frontend/';
 const npmInstall = 'npm install';
 const webappBuild = 'npm run build';
 const childProcess = require('child_process');
 const root = "./";
-
+/*
 
 childProcess.execSync(npmInstall,{cwd:frontEndFolderPath,env:process.env,stdio:'inherit'});
 childProcess.execSync(webappBuild,{cwd:frontEndFolderPath,env:process.env,stdio:'inherit'});
